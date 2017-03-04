5048a512d019cf3fe6c0b07d14d7fbd1aa93d058
Jacob Keith <jacobkeith42@gmail.com>
2017-03-04 12:35:25 -0500

    Added Stat Tracking to asses scraping performance

--- MineEmails.py
+++ MineEmails.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import urlparse
 import urllib
 import json
@@ -32,14 +33,18 @@ def scrapeBuilderPage(url):
 def scrapeBuilderWebsite(website):
 	# Goal is to find an email address
 	# print website
-	subdirs = []
+
+	hits = 0
 	print 'Scraping: ' + website
+
 
 	htmltext = urllib.urlopen(website).read()
 	soup = BeautifulSoup(htmltext, "html.parser")
+	stats['pagesScraped'] += 1
+
 	for link in soup.find_all( ['a','img'] , href=True) :
 		# Search for top level emails
-		scrapeForEmail(link)
+		hits += scrapeForEmail(link)
 		print link['href']
 		# Search for subdirectories
 
@@ -48,67 +53,85 @@ def scrapeBuilderWebsite(website):
 			print 'Scraping SubDirectory: ' + link['href']
 			htmltext = urllib.urlopen(link['href']).read()
 			soup = BeautifulSoup(htmltext, "html.parser")
+			stats['pagesScraped'] += 1
 
 			for sublink in soup.find_all('a',href=True) :
 				# Search for top level emails
-				scrapeForEmail(sublink)
+				hits += scrapeForEmail(sublink)
+				
 
-		if str(link['href']).find('contact') > -1:
+		elif str(link['href']).find('contact') > -1:
 			print 'Scraping SubDirectory: ' + website + '/' + link['href']
 			htmltext = urllib.urlopen(website + '/' + link['href']).read()
 			soup = BeautifulSoup(htmltext, "html.parser")
+			stats['pagesScraped'] += 1			
 
 			for sublink in soup.find_all('a',href=True) :
 				# Search for top level emails
-				scrapeForEmail(sublink)
+				hits += scrapeForEmail(sublink)
 
 		elif str(link['href']).find('about') > -1:
 			print 'Scraping SubDirectory: ' + website + '/' + link['href']
 			htmltext = urllib.urlopen(website + '/' + link['href']).read()
 			soup = BeautifulSoup(htmltext, "html.parser")
+			stats['pagesScraped'] += 1
 
 			for sublink in soup.find_all('a',href=True) :
 				# Search for top level emails
-				scrapeForEmail(sublink)
+				hits += scrapeForEmail(sublink)
 
 		elif str(link['href']).find('team') > -1:
 			print 'Scraping SubDirectory: ' + website + '/' + link['href']
 			htmltext = urllib.urlopen(website + '/' + link['href']).read()
 			soup = BeautifulSoup(htmltext, "html.parser")
+			stats['pagesScraped'] += 1
 
 			for sublink in soup.find_all('a',href=True) :
 				# Search for top level emails
-				scrapeForEmail(sublink)
+				hits += scrapeForEmail(sublink)
+
+	if foundEmail :
+		stats['websitesWithEmails'] += 1
 
 	return
 
 def scrapeForEmail(link) :
 	index = str(link['href']).find('mailto:')
+
 	if index > -1:
 		index2 = str(link['href']).find('@')
 		if index2 > -1:
 			emails.append(str(link['href'][7:]))
 			print 'FOUND EMAIL! : ' + str(link['href'][7:])
-	return
+			return 1
+
+	return 0
 
 
 url = "http://www.houzz.com/professionals/design-build/c/"
 city = "San Francisco"
 state = "CA"
-numPages = 20
+numPages = 1
 
 urls = []
 websites = []
 emails = []
 currentPage = 1
 
+stats = {'expectedProfiles': 0, 'profiles': 0, 'websites': 0, 'pagesScraped': 0, 'websitesWithEmails': 0, 'emails': 0}
+stats['expectedProfiles'] = numPages*15
+
 while currentPage <= numPages :
 	currentUrl = url + city + '--' + state + '/p/' + str(currentPage * 15 - 15)
 	scrapeQuery(currentUrl)
 	currentPage += 1
 
+stats['profiles'] = len(urls)
+
 for url in urls : 
 	scrapeBuilderPage(url)
+
+stats['websites'] = len(websites)
 
 for website in websites : 
 	try :
@@ -117,7 +140,22 @@ for website in websites :
 		continue
 
 cleaned = list(set(emails))
+stats['emails'] = len(cleaned)
+
 with open(city + '--' + state + '.csv', 'wb') as csvfile:
 	for e in cleaned :
 		csvfile.write(str(e) + ',\n')
 		print str(e)
+
+print ':::::Scanning Complete:::::'
+print 'Expected Number Houzz Profiles: ' + str(stats['expectedProfiles'])
+print 'Discovered Number Houzz Profiles: ' + str(stats['profiles'])
+print 'Profile Discovery Rate: ' + str(stats['profiles'] / stats['expectedProfiles'] * 100) + '%'
+print 'Number of Websites Discovered: ' + str(stats['expectedProfiles'])
+print 'Website Discovery Rate: ' + str(stats['websites'] / stats['profiles'] * 100) + '%'
+print 'Number of Pages Scraped: ' + str(stats['pagesScraped'])
+print 'Average Pages per Website: ' + str(stats['pagesScraped'] / stats['websites'])
+print 'Total Emails Discovered: ' + str(stats['emails'])
+print 'Total Websites with Emails Discovered: ' + str(stats['websitesWithEmails'])
+print 'Emails Discovery Rate per Website: ' + str(stats['emails'] / stats['websites'] * 100) + '%'
+print 'Overall Email Discovery Rate: ' + str(stats['emails'] / stats['websites'] * 100) + '%'
