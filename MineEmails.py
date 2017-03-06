691584593ab137e106ec68e75969eb41a3ff34ed
KacobJeith <jacobkeith42@gmail.com>
2017-03-05 21:45:51 -0500

    Optimized the subdirectory search, and added a logfile to keep record of your searches and the search performance

--- MineEmails.py
+++ MineEmails.py
@@ -3,6 +3,7 @@ import os
 import sys
 import urlparse
 import urllib
+import time
 import json
 from bs4 import BeautifulSoup
 
@@ -13,8 +14,6 @@ def scrapeQuery(url):
 	htmltext = urllib.urlopen(url).read()
 	soup = BeautifulSoup(htmltext, "html.parser")
 	for link in soup.find_all('a', {'class': 'pro-title'}) : 
-		# BeautifulSoup may have a bug that incorrectly grabs elements. 
-		# Check for and toss out the bad ones
 		if link['href'] != 'javascript:;' :
 			urls.append(str(link['href']))
 	return
@@ -47,54 +46,32 @@ def scrapeBuilderWebsite(website):
 		# Search for top level emails
 		hits += scrapeForEmail(link)
 		print link['href']
+
 		# Search for subdirectories
+		if any(x in str(link['href']) for x in ['contact', 'about', 'team', 'people']) : 
 
-		if str(link['href']).find(website[6:]) > -1:
-
-			print 'Scraping SubDirectory: ' + link['href']
-			htmltext = urllib.urlopen(link['href']).read()
-			soup = BeautifulSoup(htmltext, "html.parser")
-			stats['pagesScraped'] += 1
-
-			for sublink in soup.find_all('a',href=True) :
-				# Search for top level emails
-				hits += scrapeForEmail(sublink)
-				
-
-		elif str(link['href']).find('contact') > -1:
-			print 'Scraping SubDirectory: ' + website + '/' + link['href']
-			htmltext = urllib.urlopen(website + '/' + link['href']).read()
-			soup = BeautifulSoup(htmltext, "html.parser")
-			stats['pagesScraped'] += 1			
-
-			for sublink in soup.find_all('a',href=True) :
-				# Search for top level emails
-				hits += scrapeForEmail(sublink)
-
-		elif str(link['href']).find('about') > -1:
-			print 'Scraping SubDirectory: ' + website + '/' + link['href']
-			htmltext = urllib.urlopen(website + '/' + link['href']).read()
-			soup = BeautifulSoup(htmltext, "html.parser")
-			stats['pagesScraped'] += 1
-
-			for sublink in soup.find_all('a',href=True) :
-				# Search for top level emails
-				hits += scrapeForEmail(sublink)
-
-		elif str(link['href']).find('team') > -1:
-			print 'Scraping SubDirectory: ' + website + '/' + link['href']
-			htmltext = urllib.urlopen(website + '/' + link['href']).read()
-			soup = BeautifulSoup(htmltext, "html.parser")
-			stats['pagesScraped'] += 1
-
-			for sublink in soup.find_all('a',href=True) :
-				# Search for top level emails
-				hits += scrapeForEmail(sublink)
-
+			if any(x in str(link['href']) for x in ['http:', 'https:']) : 
+				hits += searchSubdirectory(link['href'])
+			else :
+				hits += searchSubdirectory( website + '/' + link['href'])
+		
 	if hits > 0 :
 		stats['websitesWithEmails'] += 1
 
 	return
+
+def searchSubdirectory(linkref) :
+	print 'Scraping SubDirectory: ' + linkref
+	htmltext = urllib.urlopen(linkref).read()
+	soup = BeautifulSoup(htmltext, "html.parser")
+	stats['pagesScraped'] += 1
+	hits = 0
+
+	for sublink in soup.find_all('a',href=True) :
+		# Search for top level emails
+		hits += scrapeForEmail(sublink)
+
+	return hits
 
 def scrapeForEmail(link) :
 	index = str(link['href']).find('mailto:')
@@ -103,11 +80,13 @@ def scrapeForEmail(link) :
 		index2 = str(link['href']).find('@')
 		if index2 > -1:
 			emails.append(str(link['href'][7:]))
-			print 'FOUND EMAIL! : ' + str(link['href'][7:])
+			print ':::::::::::::::::::::EMAIL:::::::::::::::::::::' + str(link['href'][7:])
 			return 1
 
 	return 0
 
+
+start_time = time.time()
 
 url = "http://www.houzz.com/professionals/design-build/c/"
 city = sys.argv[1]
@@ -119,7 +98,13 @@ websites = []
 emails = []
 currentPage = 1
 
-stats = {'expectedProfiles': 0, 'profiles': 0, 'websites': 0, 'pagesScraped': 0, 'websitesWithEmails': 0, 'emails': 0}
+stats = {'expectedProfiles': 0, 
+		 'profiles': 0, 
+		 'websites': 0, 
+		 'pagesScraped': 0, 
+		 'websitesWithEmails': 0, 
+		 'emails': 0}
+
 stats['expectedProfiles'] = numPages*15
 
 while currentPage <= numPages :
@@ -129,7 +114,7 @@ while currentPage <= numPages :
 
 stats['profiles'] = len(urls)
 
-for url in urls : 
+for url in urls[7:] : 
 	scrapeBuilderPage(url)
 
 stats['websites'] = len(websites)
@@ -145,22 +130,43 @@ stats['emails'] = len(cleaned)
 
 if os.path.isdir(os.path.join(os.getcwd(), 'results')) == False:
 	os.makedirs(os.path.join(os.getcwd(), 'results'))
+
+if os.path.isdir(os.path.join(os.getcwd(), 'logs')) == False:
+	os.makedirs(os.path.join(os.getcwd(), 'logs'))
+
 path = os.path.join(os.getcwd(), 'results', city + '--' + state + '.csv')
+logpath = os.path.join(os.getcwd(), 'logs', city + '--' + state + '.txt')
+
 with open(path, 'wb') as csvfile:
 	for e in cleaned :
 		csvfile.write(str(e).strip() + ',\n')
 		print str(e)
 
-print ':::::Scanning Complete:::::'
-print 'Results stored at: '+ path
-print 'Expected Number Houzz Profiles: ' + str(stats['expectedProfiles'])
-print 'Discovered Number Houzz Profiles: ' + str(stats['profiles'])
-print 'Profile Discovery Rate: ' + str(stats['profiles'] / stats['expectedProfiles'] * 100) + '%'
-print 'Number of Websites Discovered: ' + str(stats['expectedProfiles'])
-print 'Website Discovery Rate: ' + str(stats['websites'] / stats['profiles'] * 100) + '%'
-print 'Number of Pages Scraped: ' + str(stats['pagesScraped'])
-print 'Average Pages per Website: ' + str(stats['pagesScraped'] / stats['websites'])
-print 'Total Emails Discovered: ' + str(stats['emails'])
-print 'Total Websites with Emails Discovered: ' + str(stats['websitesWithEmails'])
-print 'Emails Discovery Rate per Website: ' + str(stats['emails'] / stats['websites'] * 100) + '%'
-print 'Overall Email Discovery Rate: ' + str(stats['emails'] / stats['expectedProfiles'] * 100) + '%'
+end_time = time.time()
+
+logfile = open(logpath, 'ab+')
+logfile.write(':::::::::::::::::::::' + time.strftime("%Y-%m-%d %H:%M") + ':::::::::::::::::::::' + '\n')
+logfile.write('Results stored at: '+ path + '\n')
+logfile.write('Expected Number Houzz Profiles: ' + str(stats['expectedProfiles']) + '\n')
+logfile.write('Discovered Number Houzz Profiles: ' + str(stats['profiles']) + '\n')
+logfile.write('Profile Discovery Rate: ' + str(stats['profiles'] / stats['expectedProfiles'] * 100) + '%' + '\n')
+logfile.write('Number of Websites Discovered: ' + str(stats['websites']) + '\n')
+logfile.write('Website Discovery Rate: ' + str(stats['websites'] / stats['profiles'] * 100) + '%' + '\n')
+logfile.write('Number of Pages Scraped: ' + str(stats['pagesScraped']) + '\n')
+logfile.write('Average Pages per Website: ' + str(stats['pagesScraped'] / stats['websites']) + '\n')
+logfile.write('Total Emails Discovered: ' + str(stats['emails']) + '\n')
+logfile.write('Total Websites with Emails Discovered: ' + str(stats['websitesWithEmails']) + '\n')
+logfile.write('Emails Discovery Rate per Website: ' + str(stats['websitesWithEmails'] / stats['websites'] * 100) + '%' + '\n')
+logfile.write('Overall Email Discovery Rate: ' + str(stats['websitesWithEmails'] / stats['expectedProfiles'] * 100) + '%' + '\n')
+logfile.write('Total Runtime: %g seconds' % (end_time - start_time)  + '\n')
+logfile.write('Runtime per Website: ' + str((end_time - start_time) / stats['websites']) + ' seconds' + '\n\n')
+
+
+
+
+
+
+
+
+
+
